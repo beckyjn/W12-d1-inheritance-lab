a8300bd52ceaa08063158487b432c3d616322da5
beckyjn <rebeccajoynielsen@gmail.come>
2019-05-13 13:47:09 +0100

    manager class complete

---
+++ src/main/java/Staff/Employee.java
@@ -0,0 +1,35 @@
+package Staff;
+
+public abstract class Employee {
+
+    private String name;
+    private String niNumber;
+    private double salary;
+
+    public Employee(String name, String niNumber, double salary) {
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNiNumber() {
+        return niNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double increment){
+        double raise = salary * increment;
+        salary += raise;
+    }
+
+    public double payBonus() {
+        return salary * 0.01;
+    }
+}
---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,46 @@
+import Staff.Employee;
+import Staff.Management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+    Manager manager;
+
+    @Before
+    public void before() {
+        manager = new Manager("Becky", "5B", 50000.0, "Cool Stuff");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Becky", manager.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals("5B", manager.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(50000.0, manager.getSalary(), 0.1);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        manager.raiseSalary(0.1);
+        assertEquals(55000, manager.getSalary(), 0.1);
+    }
+
+    @Test
+    public void canGiveBonus() {
+        assertEquals(500, manager.payBonus(), 0.1);
+    }
+
+    @Test
+    public void hasDepartmentName() {
+        assertEquals("Cool Stuff", manager.getDeptName());
+    }
+}
